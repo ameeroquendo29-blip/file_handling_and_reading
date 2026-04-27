a262a7936a2f5da25767994d3dce8700c553e9c0
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-04-28 00:33:23 +0800

    put the input system and the exceptions

--- file_handling_and_reading/input_sentences_and_put_in_into_text_file/input_sentences_and_put_in_into_text_file.py
+++ file_handling_and_reading/input_sentences_and_put_in_into_text_file/input_sentences_and_put_in_into_text_file.py
@@ -1,3 +1,17 @@
 class InputMultipleLines:
     def __init__(self, filename: str = "mylife.txt"):
         self.filename = filename
+
+    def input_lines(self):
+        try:
+            with open(self.filename, "a") as file:
+                while True:
+                    line = input("Enter a line: ")
+                    more_lines = input("Are there any more lines (y/n)? ").lower()
+                    file.write(f"{text}\n")
+                    if more_lines != "y":
+                        break
+        except FileNotFoundError:
+            print("File not found.")
+        except:
+            print("Error in writing the file.")
