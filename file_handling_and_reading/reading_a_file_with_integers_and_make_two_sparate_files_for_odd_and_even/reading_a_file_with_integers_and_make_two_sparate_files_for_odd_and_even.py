a382367302e2d900c5541ed316dacf008506d02f
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-04-27 15:39:18 +0800

    file reading

--- file_handling_and_reading/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even.py
+++ file_handling_and_reading/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even.py
@@ -1,3 +1,10 @@
 class EvenOddSeparator:
     def __init__(self, filename: str = "./numbers.txt"):
         self.filename = filename
+
+    def file_read(self) -> list[int]:
+        try:
+            with open(self.filename, r) as file:
+                numbers = [int(number.rstrip("\n")) for number in file.readlines()
+            return numbers
+
