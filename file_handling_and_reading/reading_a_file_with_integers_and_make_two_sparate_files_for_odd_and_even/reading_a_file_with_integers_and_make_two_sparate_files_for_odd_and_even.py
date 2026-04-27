e92386287cf2b3dd91cadd5bf0e57a4676259613
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-04-27 16:14:30 +0800

    added another exceptions and fix all of other problems in the code

--- file_handling_and_reading/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even.py
+++ file_handling_and_reading/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even/reading_a_file_with_integers_and_make_two_sparate_files_for_odd_and_even.py
@@ -4,14 +4,16 @@ class EvenOddSeparator:
 
     def file_read(self) -> list[int]:
         try:
-            with open(self.filename, r) as file:
-                numbers = [int(number.rstrip("\n")) for number in file.readlines()
+            with open(self.filename, "r") as file:
+                numbers = [int(number.rstrip("\n")) for number in file.readlines()]
             return numbers
 
         except FileNotFoundError:
             print(f"File {self.filename} doesn't exist")
+            return []
         except:
             print("Make sure the file only contains integers")
+            return []
 
     def file_write(self, filename: str, content: int):
         with open(filename, "a") as file:
@@ -19,11 +21,13 @@ class EvenOddSeparator:
 
     def categorize(self):
         data = self.file_read()
+        open("even.txt", "w").close()
+        open("odd.txt", "w").close()
         for number in data:
             if number % 2 == 0:
-                self.file_write(even.txt, number)
+                self.file_write("even.txt", number)
             else:
-                self.file_write(odd.txt, number)
+                self.file_write("odd.txt", number)
 
 if __name__ == "__main__":
     separator = EvenOddSeparator()
