56e4cec2c1b172d41cb5d01e1c13f6b59cb70f40
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-04-28 00:45:06 +0800

    created the class and started file naming

---
+++ file_handling_and_reading/reading_a_file_with_integers_and_create_two_files_with_the_sqaure_and_cube_of_the_integers/reading_a_file_with_integers_and_create_two_files_with_the_sqaure_and_cube_of_the_integers.py
@@ -0,0 +1,5 @@
+class SquareCubeOfTheIntegers:
+    def __init__(self, filename: str = "integers.txt"):
+        self.filename = filename
+        self.square_integers = "double.txt"
+        self.cube_integers = "triple.txt"
