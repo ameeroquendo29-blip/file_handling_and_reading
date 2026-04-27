f005fb3375fa74c123f175622580811fb3edb5df
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-04-27 21:55:42 +0800

    file reading

--- file_handling_and_reading/reading_a_file_with_student_name_and_GWA_then_present_the_name_with_highest_GWA/reading_a_file_with_student_name_and_gwa_then_present_the_name_with_highest_gwa.py
+++ file_handling_and_reading/reading_a_file_with_student_name_and_GWA_then_present_the_name_with_highest_GWA/reading_a_file_with_student_name_and_gwa_then_present_the_name_with_highest_gwa.py
@@ -1,3 +1,8 @@
 class HighestGwa:
     def __init__(self, filename: str = "gwa.txt"):
         self.filename = filename
+
+    def file_read(self) -> list[str]:
+        with open(self.filename, "r") as file:
+            content = [line.rstrip("\n") for line in file.readlines()]
+        return content
